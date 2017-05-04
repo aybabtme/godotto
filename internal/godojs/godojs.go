4f3f1d74aa5d1952ad60e3114052ccd6126125f3
Iheanyi Ekechukwu <iheanyi@digitalocean.com>
2017-05-04 09:54:14 -0400

    Make region slug mandatory in update request.

--- internal/godojs/godojs.go
+++ internal/godojs/godojs.go
@@ -181,7 +181,7 @@ func ArgLoadBalancerUpdate(vm *otto.Otto, v otto.Value) *godo.LoadBalancerReques
 	return &godo.LoadBalancerRequest{
 		Name:                ottoutil.String(vm, ottoutil.GetObject(vm, v, "name", true)),
 		Algorithm:           ottoutil.String(vm, ottoutil.GetObject(vm, v, "algorithm", false)),
-		Region:              ArgRegionSlug(vm, ottoutil.GetObject(vm, v, "region", false)),
+		Region:              ArgRegionSlug(vm, ottoutil.GetObject(vm, v, "region", true)),
 		DropletIDs:          ArgDropletIDs(vm, ottoutil.GetObject(vm, v, "droplet_ids", false)),
 		HealthCheck:         ArgHealthCheck(vm, ottoutil.GetObject(vm, v, "health_check", false)),
 		StickySessions:      ArgStickySessions(vm, ottoutil.GetObject(vm, v, "sticky_sessions", false)),
